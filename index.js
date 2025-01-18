07be52f8fa3ab7a3c54348ea88d9e50448ad602d
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 14:04:35 +0000

    Create graph task

---
+++ index.js
@@ -0,0 +1,30 @@
+class Graph {
+    constructor() {
+
+    }
+
+    // Add a new vertex to the graph
+    addVertex(vertex) {
+       
+    }
+
+    // Add an edge between two vertices
+    addEdge(vertex1, vertex2) {
+        
+    }
+
+    // Remove a vertex from the graph
+    removeVertex(vertex) {
+        
+    }
+
+    // Remove an edge between two vertices
+    removeEdge(vertex1, vertex2) {
+        
+    }
+
+    // Display the graph structure
+    display() {
+        
+    }
+}
